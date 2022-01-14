35b528def00867566edea3406fc64c10dc6d9614
Oleksii Matviichuk <77227774+AlexDvor@users.noreply.github.com>
2022-01-14 17:20:39 +0100

    finished form

--- src/App.js
+++ src/App.js
@@ -1,18 +1,7 @@
 import './App.css';
 
-import SignInForm from './components/SignInForm/SignInForm';
-import Container from './components/Container/Container';
-import LoginForm from './components/LoginForm/LoginForm';
-
 function App() {
-  return (
-    <Container>
-      <div>
-        <SignInForm></SignInForm>
-        <LoginForm></LoginForm>
-      </div>
-    </Container>
-  );
+  return <></>;
 }
 
 export default App;
