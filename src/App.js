9b1c50b3b4bfbe5d8f78b2b0a7fd3705ab277d25
OksanaMosendz <77731512+OksanaMosendz@users.noreply.github.com>
2022-01-15 17:14:31 +0200

    finance table, summary

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import './App.css';
 
 import LoginForm from './components/LoginForm/LoginForm';
 import Container from './components/Container/Container';
+import FinanceTable from './components/FinanceTable/FinanceTable' ;
+import Summary from './components/Summary/Summary';
 
 function App() {
   return (
     <Container>
       <LoginForm></LoginForm>
+      <FinanceTable/>
+      <Summary/>
     </Container>
   );
 }
---
+++ src/components/Summary/Summary.styled.jsx
@@ -0,0 +1,34 @@
+import styled from '@emotion/styled';
+
+export const SummaryTable = styled.table`
+background-color: #F5F6FB;
+border-collapse: collapse;
+width: 100%;
+`
+export const SummaryWrapper = styled.div`
+border-radius: 20px 20px 20px 0;
+  overflow: hidden;
+  width: 230px;
+ text-align: center;
+  font-family: Roboto;
+font-size: 12px;
+line-height: 1.16;
+align-items: center;
+letter-spacing: 0.04em;
+text-transform: uppercase;
+color: #52555F;
+`
+export const StyledTr = styled.tr`
+border: 2px solid #ffffff;
+height: 38px;
+`
+export const HeadTd = styled.td`
+font-family: Roboto;
+font-style: normal;
+font-weight: 700;
+font-size: 12px;
+letter-spacing: 0.02em;
+color: #000000;
+justify-self: center;
+`
+
