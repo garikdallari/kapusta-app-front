97c19939ac9e79e1a596474b77a5fd0f997ed29a
valeraron <77055501+valeraron@users.noreply.github.com>
2022-01-15 19:31:21 +0200

    remote Modal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,22 +1,40 @@
+import React, { useEffect } from 'react';
 import { GrClose } from 'react-icons/gr';
 import {
-  ModalConteiner,
+  ModalContainer,
   ModalContent,
   ModalText,
   ButtonContainer,
   CloseButton,
 } from './Modal.styled';
+import Button from '../Button/Button';
 
-export const Modal = ({ text, textColor, children, onClick }) => {
+export const Modal = ({ text, textColor, showModal }) => {
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  });
+
+  const handleKeyDown = e => {
+    if (e.code === 'Escape') {
+      showModal();
+    }
+  };
+
   return (
-    <ModalConteiner>
+    <ModalContainer>
       <ModalContent>
         <ModalText textColor={textColor}>{text}</ModalText>
-        <ButtonContainer>{children}</ButtonContainer>
+        <ButtonContainer>
+          <Button text="Да" type="button" />
+          <Button text="Нет" type="button" />
+        </ButtonContainer>
       </ModalContent>
-      <CloseButton onClick={onClick}>
+      <CloseButton type="button" onClick={showModal}>
         <GrClose size={12} />
       </CloseButton>
-    </ModalConteiner>
+    </ModalContainer>
   );
 };
