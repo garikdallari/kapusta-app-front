54b89a5e61f9c03197e6a3961a79e1acb3769f08
Oleksii Matviichuk <77227774+AlexDvor@users.noreply.github.com>
2022-01-17 10:42:25 +0100

    save changes

--- src/App.js
+++ src/App.js
@@ -1,42 +1,39 @@
 import { Switch, Redirect, Route } from 'react-router-dom';
 import './App.css';
 
-// import PublicRoute from './routes/PublicRoute';
-// import PrivateRoute from './routes/PrivateRoute';
+import PublicRoute from './routes/PublicRoute';
+import PrivateRoute from './routes/PrivateRoute';
 
 import SignupPage from './pages/SignupPage/SignupPage';
-// import LoginPage from './pages/LoginPage/LoginPage';
-// import ReportPage from './pages/ReportPage/ReportPage';
-// import HomePage from './pages/HomePage/HomePage';
+import LoginPage from './pages/LoginPage/LoginPage';
+import ReportPage from './pages/ReportPage/ReportPage';
+import HomePage from './pages/HomePage/HomePage';
 
 function App() {
   return (
-    <>
-      <SignupPage></SignupPage>
-    </>
-    // <Switch>
-    //   <Route path="/" exact>
-    //     <Redirect to="/login" />
-    //   </Route>
+    <Switch>
+      <Route path="/" exact>
+        <Redirect to="/login" />
+      </Route>
 
-    //   <Route path="/signup" restricted>
-    //     <SignupPage />
-    //   </Route>
+      <Route path="/signup" restricted>
+        <SignupPage />
+      </Route>
 
-    //   <Route path="/login" redirectTo="/home" restricted>
-    //     <LoginPage />
-    //   </Route>
+      <Route path="/login" redirectTo="/home" restricted>
+        <LoginPage />
+      </Route>
 
-    //   <Route path="/home">
-    //     <HomePage />
-    //   </Route>
+      <Route path="/home">
+        <HomePage />
+      </Route>
 
-    //   <Route path="/report">
-    //     <ReportPage />
-    //   </Route>
+      <Route path="/report">
+        <ReportPage />
+      </Route>
 
-    //   <Redirect to="/" />
-    // </Switch>
+      <Redirect to="/" />
+    </Switch>
   );
 }
 
