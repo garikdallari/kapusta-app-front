01fc76d13500a8e1791ff3d3569070339fb4340a
OksanaMosendz <77731512+OksanaMosendz@users.noreply.github.com>
2022-01-18 16:47:40 +0200

    add deleteTransaction

--- src/redux/auth/auth-selectors.js
+++ src/redux/auth/auth-selectors.js
@@ -2,9 +2,12 @@ const getIsLoggedIn = state => state.auth.IsLoggedIn;
 
 const getUsername = state => state.auth.user.name;
 
+const getToken = state => state.auth.user.token;
+
 const authSelectors = {
   getIsLoggedIn,
   getUsername,
+  getToken
 };
 
 export default authSelectors;
