1b0c241156a97ca2def04d5b5ab61f9dd4342db2
ASTOUND\o.myronenko <o.myronenko@astoundcommerce.com>
2022-01-19 21:31:40 +0200

    feature/switch-categories: done switching categories

--- src/pages/ReportPage/ReportPage.jsx
+++ src/pages/ReportPage/ReportPage.jsx
@@ -1,3 +1,6 @@
+import { useState, useEffect } from 'react';
+import { Context } from '../../components/Context/Context';
+import { useSelector } from 'react-redux';
 import {
   WrapperBackgroundBody,
   BackgroundBodyReport,
@@ -9,42 +12,56 @@ import Accounting from '../../components/Accounting/Accounting';
 import ReportBalance from '../../components/Balance/ReportBalance';
 import IconsReportSection from '../../components/IconsReportSection/IconsReportSection';
 import { NavBox, ReportContainer } from './ReportPage.styled';
-import { theme } from '../../constants/theme';
+
+// удалить массив categories, перед подключением store
+const expenseArray = [
+  { key: 1, name: 'products', price: '1000' },
+  { key: 2, name: 'cocktail', price: '1000' },
+  { key: 3, name: 'fun', price: '10' },
+  { key: 4, name: 'health', price: '1000' },
+  { key: 5, name: 'car', price: '1000' },
+  { key: 6, name: 'household', price: '1000' },
+  { key: 7, name: 'tools', price: '1000' },
+  { key: 8, name: 'invoice', price: '10' },
+  { key: 11, name: 'hobby', price: '1000' },
+  { key: 9, name: 'book', price: '1000' },
+  { key: 10, name: 'other', price: '10' },
+];
+
+const incomeArray = [
+  { key: 1, name: 'salary', price: '1000' },
+  { key: 2, name: 'extra', price: '1000' },
+];
 
 export default function ReportPage() {
-  // удалить массив categories, перед подключением store
-  const categories = [
-    { key: 1, name: 'products', price: '1000' },
-    { key: 2, name: 'cocktail', price: '1000' },
-    { key: 3, name: 'kite', price: '10' },
-    { key: 4, name: 'health', price: '1000' },
-    { key: 5, name: 'car', price: '1000' },
-    { key: 6, name: 'couch', price: '1000' },
-    { key: 7, name: 'tools', price: '1000' },
-    { key: 8, name: 'invoice', price: '10' },
-    { key: 11, name: 'hobby', price: '1000' },
-    { key: 9, name: 'book', price: '1000' },
-    { key: 10, name: 'ufo', price: '10' },
-  ];
+  const [sectionState, setSectionState] = useState(null);
+  const [context, setContext] = useState('expense');
+  // const getAllByMonth = useSelector(state => state.transactions.getAllByMonth);
+
+  useEffect(() => {
+    setSectionState(context === 'expense' ? expenseArray : incomeArray);
+  }, [context]);
   return (
     <>
-      <WrapperBackgroundBody>
-        <BackgroundBodyReport>
-          <Container>
-            <ReportContainer>
-              <NavBox>
-                <ButtonArrowBack
-                  // displayBtn={'inline-block'}
-                  displayText={'block'}
-                />
-                <ReportBalance />
-              </NavBox>
-              <Accounting />
-              <IconsReportSection amount={categories} />
-            </ReportContainer>
-          </Container>
-        </BackgroundBodyReport>
-      </WrapperBackgroundBody>
+      <Context.Provider value={[context, setContext]}>
+        <WrapperBackgroundBody>
+          <BackgroundBodyReport>
+            <Container>
+              <ReportContainer>
+                <NavBox>
+                  <ButtonArrowBack
+                    // displayBtn={'inline-block'}
+                    displayText={'block'}
+                  />
+                  <ReportBalance />
+                </NavBox>
+                <Accounting />
+                <IconsReportSection amount={sectionState} />
+              </ReportContainer>
+            </Container>
+          </BackgroundBodyReport>
+        </WrapperBackgroundBody>
+      </Context.Provider>
     </>
   );
 }
