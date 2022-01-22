007b2d564056a275c116e85a57d67857526571ab
Dasha Kirienko <dkirienko25@gmail.com>
2022-01-22 17:27:17 +0300

    add balance refresh

--- src/components/Balance/HomeBalance.jsx
+++ src/components/Balance/HomeBalance.jsx
@@ -1,5 +1,10 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import {
+  getUserBalance,
+  updateUserBalance,
+} from '../../redux/balance/balance-operations.js';
+import { getBalance } from '../../redux/balance/balance-selectors.js';
 
 import {
   WrapperInput,
@@ -15,16 +20,55 @@ import BalanceNotification from '../BalanceNotification/BalanceNotification';
 import ReportsNavigation from '../ReportsNavigation/ReportsNavigation';
 
 export default function HomeBalance() {
+  const currentBalance = useSelector(getBalance);
+  const dispatch = useDispatch();
+  const [balance, setBalance] = useState();
+  let btnText = '';
+
+  if (currentBalance === 0) {
+    btnText = 'confirm';
+  } else {
+    btnText = 'renew';
+  }
+
+  useEffect(() => {
+    dispatch(getUserBalance());
+    setBalance(currentBalance);
+  }, [dispatch, currentBalance]);
+
+  const handleInput = e => {
+    const { value } = e.currentTarget;
+    setBalance(value);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (currentBalance === 0) {
+      dispatch(updateUserBalance({ balance }));
+      dispatch(getUserBalance());
+      setBalance(currentBalance);
+    } else {
+      dispatch(getUserBalance());
+      setBalance(currentBalance);
+    }
+  };
+
   return (
     <>
       <WrapperInput>
         <ReportsNavigation />
         <BalanceBox>
           <BalanceTitle>Balance:</BalanceTitle>
-          <InputContainer>
+
+          <InputContainer onSubmit={handleSubmit}>
             <Label>$</Label>
-            <BalanceInput />
-            <BalanceButton type="submit">Confirm</BalanceButton>
+            <BalanceInput
+              name="balance"
+              value={balance}
+              onChange={handleInput}
+              required
+            />
+            <BalanceButton type="submit">{btnText}</BalanceButton>
           </InputContainer>
           {/* <BalanceNotification /> */}
         </BalanceBox>
--- src/redux/balance/balance-operations.js
+++ src/redux/balance/balance-operations.js
@@ -1,16 +1,27 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import * as balanceApi from '../../services/balanceAPI';
 
-const getUserBalance = createAsyncThunk('auth/getUserBalance', async () => {
-  const balance = await balanceApi.getUserBalance();
-  return balance.data.result;
-});
+const getUserBalance = createAsyncThunk(
+  'auth/getUserBalance',
+  async (_, { rejectWithValue }) => {
+    try {
+      const balance = await balanceApi.getUserBalance();
+      return balance;
+    } catch (error) {
+      rejectWithValue(error.message);
+    }
+  },
+);
 
 const updateUserBalance = createAsyncThunk(
   'auth/updateUserBalance',
-  async balance => {
-    const updatedBalance = await balanceApi.updateBalance(balance);
-    return updatedBalance.data;
+  async ({ balance }, { rejectWithValue }) => {
+    try {
+      const updatedBalance = await balanceApi.updateBalance({ balance });
+      return updatedBalance;
+    } catch (error) {
+      rejectWithValue(error.message);
+    }
   },
 );
 
