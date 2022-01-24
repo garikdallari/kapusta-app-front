dd6549dc487d9ce18f88f0112249505ac631cf95
MaksymPryhorko <78167917+MaksymPryhorko@users.noreply.github.com>
2022-01-24 23:33:21 +0200

    Убрал скролл инпутСелекта.

---
+++ src/components/InputSelect/data.jsx
@@ -0,0 +1,18 @@
+export const expenseData = [
+  { value: 'Transport', label: 'Transport' },
+  { value: 'Food', label: 'Food' },
+  { value: 'Health', label: 'Health' },
+  { value: 'Alcohol', label: 'Alcohol' },
+  { value: 'Activities', label: 'Activities' },
+  { value: 'Home stuff', label: 'Home stuff' },
+  { value: 'Gadgets', label: 'Gadgets' },
+  { value: 'Utility bills', label: 'Utility bills' },
+  { value: 'Hobbies', label: 'Hobbies' },
+  { value: 'Education', label: 'Education' },
+  { value: 'Others', label: 'Others' },
+];
+
+export const incomeData = [
+  { value: 'Salary', label: 'Salary' },
+  { value: 'Extra Income', label: 'Extra Income' },
+];
