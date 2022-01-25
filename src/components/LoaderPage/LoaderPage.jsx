354432746e443d9c8c5ecf9524bc359161487083
Oleksii Matviichuk <77227774+AlexDvor@users.noreply.github.com>
2022-01-25 16:56:56 +0100

    refactoring loader

--- src/components/LoaderPage/LoaderPage.jsx
+++ src/components/LoaderPage/LoaderPage.jsx
@@ -1,24 +1,26 @@
 import React from 'react';
 import ContentLoader from 'react-content-loader';
-import { BackGroundBody } from './LoaderPage.styled';
+import { Overlay, WrapperOverlay } from './LoaderPage.styled';
 
 const Loader = props => (
   <>
-    <BackGroundBody>
-      <ContentLoader
-        viewBox="0 0 400 160"
-        height={100}
-        width={400}
-        speed={1}
-        backgroundColor="#fb7c2f"
-        foregroundColor="#f5f6fb"
-        {...props}
-      >
-        <circle cx="150" cy="86" r="8" />
-        <circle cx="194" cy="86" r="8" />
-        <circle cx="238" cy="86" r="8" />
-      </ContentLoader>
-    </BackGroundBody>
+    <Overlay>
+      <WrapperOverlay>
+        <ContentLoader
+          viewBox="0 0 400 160"
+          height={100}
+          width={400}
+          speed={1}
+          backgroundColor="#fb7c2f"
+          foregroundColor="#f5f6fb"
+          {...props}
+        >
+          <circle cx="150" cy="86" r="8" />
+          <circle cx="194" cy="86" r="8" />
+          <circle cx="238" cy="86" r="8" />
+        </ContentLoader>
+      </WrapperOverlay>
+    </Overlay>
   </>
 );
 
--- src/components/LoaderPage/LoaderPage.styled.jsx
+++ src/components/LoaderPage/LoaderPage.styled.jsx
@@ -1,22 +1,44 @@
 import styled from '@emotion/styled';
 
-export const BackGroundBody = styled.div`
-  justify-content: center;
-  display: flex;
-  align-items: center;
-  background-color: ${props => props.theme.backgroundBodyColor};
-  max-width: 767px;
-  height: 286px;
-  border-radius: 0px 0px 0px 80px;
-  padding: ${props => props.padding};
+// export const BackGroundBody = styled.div`
+//   justify-content: center;
+//   display: flex;
+//   align-items: center;
+//   background-color: ${props => props.theme.backgroundBodyColor};
+//   max-width: 767px;
+//   height: 286px;
+//   border-radius: 0px 0px 0px 80px;
+//   padding: ${props => props.padding};
 
-  @media (min-width: 320px) {
-    max-width: 767px;
-    height: 286px;
-  }
+//   @media (min-width: 320px) {
+//     max-width: 767px;
+//     height: 286px;
+//   }
 
-  @media (min-width: 768px) {
-    max-width: 2280px;
-    height: 526px;
-  }
+//   @media (min-width: 768px) {
+//     max-width: 2280px;
+//     height: 526px;
+//   }
+// `;
+
+export const Overlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: transparent;
+  z-index: 10;
+  opacity: 1;
+  transition: opacity 250ms cubic-bezier(0.4, 0, 0.2, 1),
+    visibility 250ms cubic-bezier(0.4, 0, 0.2, 1);
+`;
+
+export const WrapperOverlay = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  background-color: transparent;
+  transform: translate(-50%, -50%) scale(1);
+  transition: transform 250ms cubic-bezier(0.4, 0, 0.2, 1);
 `;
